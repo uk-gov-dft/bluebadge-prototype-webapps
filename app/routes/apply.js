52c9c0595422a72cde6c78543ed11632d1ab7d99
charge-valtech <henry.charge@valtech.co.uk>
2018-09-26 15:05:56 +0100

    New flow and question set for walking

--- app/routes/apply.js
+++ app/routes/apply.js
@@ -649,7 +649,7 @@ router.get('/prove-eligibility/list-mobility-aids', function(req, res) {
   }
 
   res.locals.tableRows = tableRows;
-  Object.assign(res.locals,sendBackToCheckAnswers(req.query,'/apply-for-a-blue-badge/prove-eligibility/walking-time','check-walking'))
+  Object.assign(res.locals,sendBackToCheckAnswers(req.query,'/apply-for-a-blue-badge/prove-eligibility/mobility-aid-backend','check-walking'))
   res.render(proveEligibilityTemplatePath+'list-mobility-aids');
 });
 
@@ -663,8 +663,11 @@ router.get('/prove-eligibility/add-mobility-aid', function(req, res) {
 });
 
 router.get('/prove-eligibility/create-mobility-aid', function(req, res) {
+  var aidName = (req.session.data['mobility-aid-type'] === 'walking-aid') ? 
+                'Walking aid (' + req.session.data['mobility-aid-name'] + ')' : req.session.data['mobility-aid-type'];
+
   var mobilityAid = {
-    "name": req.session.data['mobility-aid-name'],
+    "name": aidName,
     "usage": req.session.data['mobility-aid-usage'],
     "source": req.session.data['mobility-aid-source']
   }
@@ -677,7 +680,7 @@ router.get('/prove-eligibility/create-mobility-aid', function(req, res) {
 
   var changeValue = req.query.change == 'true' ? '?change=true' : '';
 
-  delete req.session.data['mobility-aid-name','mobility-aid-usage','mobility-aid-source'];
+  delete req.session.data['mobility-aid-type','mobility-aid-name','mobility-aid-usage','mobility-aid-source'];
   res.redirect('/apply-for-a-blue-badge/prove-eligibility/list-mobility-aids' + changeValue);
 });
 
@@ -694,16 +697,32 @@ router.get('/prove-eligibility/walking-time', function(req, res) {
   res.render(proveEligibilityTemplatePath+'walking-time');
 });
 
-router.get('/prove-eligibility/walking-time-backend', function(req, res) {
-  if (req.session.data['how-long-walk'] === 'cant-walk') {
-    res.redirect(proveEligibilityPath+'check-walking');
+router.get('/prove-eligibility/mobility-aid-backend', function(req, res) {
+  if(req.session.data['mobility-aids-array']) {
+    var aidsArray = req.session.data['mobility-aids-array'],
+        arrayLength = aidsArray.length,
+        aidName = '';
+
+    for (var i = 0; i < arrayLength; i++) {
+      aidName = aidsArray[i].name;
+    }
+
+    if (aidName.includes('Walking aid')) {
+      res.redirect(proveEligibilityPath+'where-can-you-walk');
+      req.session.data['can-walk'] = 'yes';
+    } else {
+      req.session.data['can-walk'] = 'no';
+      res.redirect(proveEligibilityPath+'check-walking');
+    }
   } else {
     res.redirect(proveEligibilityPath+'where-can-you-walk');
   }
+
+  
 });
 
 router.get('/prove-eligibility/where-can-you-walk', function(req, res) {
-  Object.assign(res.locals,sendBackToCheckAnswers(req.query,'/apply-for-a-blue-badge/prove-eligibility/how-quickly-do-you-walk','check-walking'))
+  Object.assign(res.locals,sendBackToCheckAnswers(req.query,'/apply-for-a-blue-badge/prove-eligibility/check-walking','check-walking'))
   res.render(proveEligibilityTemplatePath+'where-can-you-walk');
 });
 
